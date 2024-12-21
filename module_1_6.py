f7d4c2f5b3d69b1504c34f1912fde4410e0a84b1
Джони <Jonycoach1989@gmail.com>
2024-12-21 12:51:18 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Jony' : 1989, 'Dima' : 1987}
+print(my_dict)
+print(my_dict.get('Jony'), ('Homie'))
+my_dict.update({'Tom' : 1991,
+                'Rachel' : 1990})
+del my_dict['Tom']
+print(my_dict)
+my_set_ = {1, 2, 3, 5, 5, 8, 1, 7, 'String', True}
+print(my_set_)
+my_set_.add(16)
+my_set_.add((1, 2, 3))
+print(my_set_)
+my_set_.remove(3)
+print(my_set_)
